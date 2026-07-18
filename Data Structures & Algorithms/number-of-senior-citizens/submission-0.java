cefc16a9482d0a3a9af4f39bdbd13c11a05a8b6b
Prince Raj <ayansprince001@gmail.com>
2026-07-18 14:06:52 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+       int count=0;
+        for(int i=0;i<details.length;i++){
+            if((details[i].charAt(11)>'6' || details[i].charAt(12)>'0' &&  details[i].charAt(11)=='6')){
+                count ++;
+            }
+
+        }
+        return count;
+        
+    }
+}
